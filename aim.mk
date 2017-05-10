49c74702ac088e93f75a45611317d7b189052f42
klabit87 <keith.labit@gmail.com>
2017-05-09 21:36:23 -0500

    AIM-ify

--- aim.mk
+++ aim.mk
@@ -1,11 +1,11 @@
 $(call inherit-product, device/htc/himawl/full_himawl.mk)
 
 # Inherit some common Lineage stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+$(call inherit-product, vendor/aim/config/common_full_phone.mk)
 
 PRODUCT_GMS_CLIENTID_BASE := android-verizon
 
-PRODUCT_NAME := lineage_himawl
+PRODUCT_NAME := aim_himawl
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_DEVICE="htc_himawl" \
